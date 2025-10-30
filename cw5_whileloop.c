9d327a5e6f59d48b300ce98c8a2b4f9a4f9d9e88
cong.le <cong.le@tcu.edu>
2025-10-30 13:58:10 -0500

    Class work 5

---
+++ cw5_whileloop.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main() {
+	int numCustomer, i=0, unit;
+	float currentBill = 0, avgBill=0, highestBill=0, lowestBill=0, totalBill = 0;
+	printf("Enter number of customer: ");
+	scanf("%d", &numCustomer);
+	
+	while (i <numCustomer) {
+		i++
+		currentBill = 0;
+		printf("Enter the number of electricity units consumed: ");
+		scanf("%d", &unit);
+
+		if (unit >0) {
+			if (unit <100) {
+				currentBill+= unit*0.5;				
+			} else {
+				if (unit<300) {
+					currentBill+=100*0.5+(unit-100)*0.75;
+				} else {
+					if (unit<600) {
+						currentBill+=100*0.5+200*0.75+(unit-300)*1.2;
+					} else {
+						currentBill+=100*0.5+200*0.75+300*1.2+(unit-600)*1.5;
+					}
+				}
+			}
+			 
+		}			
+		currentBill +=20;
+		
+		if (currentBill >500){
+			currentBill=currentBill*1.1;
+		}
+		
+		printf("Total bill: %.2f \n", currentBill);
+		
+		if (highestBill < currentBill) {
+			highestBill = currentBill;
+		}
+		if (lowestBill > currentBill && lowestBill > 0) {
+			lowestBill = currentBill;
+		}
+		totalBill+=currentBill;
+	}
+	avgBill = totalBill/numCustomer;
+	printf("Average bill amount: %.2f \n", avgBill);
+
+	printf("Highest bill amount: %.2f \n", highestBill);
+	printf("Lowest bill amount: %.2f \n", lowestBill);
+}
+
